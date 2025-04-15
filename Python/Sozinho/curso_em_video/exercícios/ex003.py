c59167431e9826ee07c21a8276d08d00c841587d
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-04-15 17:48:36 -0300

    Add files via upload

---
+++ Python/Sozinho/curso_em_video/exercícios/ex003.py
@@ -0,0 +1,7 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles
+
+primeiro_numero = int(input('Digite um valor inteiro: '   ))
+segundo_numero  = int(input('Digite outro valor inteiro: '))
+soma = primeiro_numero + segundo_numero
+
+print(f'{primeiro_numero} + {segundo_numero} = {soma}')
