271c44cdc780c7dbe21177bbdadba3414cfa935d
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-05-06 17:18:26 -0300

    Create ex006 - - Dobro, Triplo, Raiz Quadrada.py

---
+++ Python/Sozinho/curso_em_video/exercícios/ex006 - - Dobro, Triplo, Raiz Quadrada.py
@@ -0,0 +1,3 @@
+Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+n = (int(input('Digite um número: ')))
+print(f'\nDobro: {n*2}\nTriplo: {n*3}\nRaíz Quadrada: {n**0.5}')
