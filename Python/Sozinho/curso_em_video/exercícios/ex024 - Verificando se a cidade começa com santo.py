5a816b149a615ff0180b764a2adc29ece9ef52ff
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-05-20 18:25:06 -0300

    Add files via upload

---
+++ Python/Sozinho/curso_em_video/exercícios/ex024 - Verificando se a cidade começa com santo.py
@@ -0,0 +1,7 @@
+cidade = input('Qual o nome da sua cidade?\n').upper().strip()
+santiCidade = (cidade[:5] == 'SANTO')
+if santiCidade:
+    print(f'Sua cidade, {cidade}, começa com "Santo".')
+else:
+    print(f'Sua cidade, {cidade}, não começa com "Santo".')
+#print(cidade)
