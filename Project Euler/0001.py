17ee59d0b510b552812c5f864d020a1a5bcf3514
Ryan Guilherme <ryanguilherme2003@gmail.com>
2025-06-02 18:14:26 -0300

    Delete Project Euler/0001.py

--- Project Euler/0001.py
+++
@@ -1,7 +0,0 @@
-# Find the sum of all the multiples of Multiples of 3 or 5 bellow 1000
-soma = 0
-for n in range(0,1000):
-    if n % 3 == 0 or n % 5 == 0:
-        soma += n
-        print(n)
-print(soma)
