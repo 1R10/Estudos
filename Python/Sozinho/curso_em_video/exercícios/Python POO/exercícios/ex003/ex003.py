ead3469bad27c2cc27909248077505e4fd8bcfd0
1R10 <ryanguilherme2003@gmail.com>
2026-02-18 19:57:32 -0300

    att ex03

--- Python/Sozinho/curso_em_video/exercícios/Python POO/exercícios/ex003/ex003.py
+++ Python/Sozinho/curso_em_video/exercícios/Python POO/exercícios/ex003/ex003.py
@@ -1,5 +1,15 @@
 class Conta_Bancaria:
     '''
-    Conta Bancária né papai 
+    Cria uma conta bancária e permite fazer saques/depósitos
     '''
     #Continuar de 29:00
+    def __init__(self, id, nome, saldo = 0):
+        self.id      = id
+        self.titular = nome
+        self.saldo   = saldo
+    def __str__(self):
+        return f"A conta {self.id}, do titular {self.titular}, possui um saldo de R${self.saldo:.2f}"
+
+# Área do uso da classe # ------------------------------------------------------------------------------------------
+conta_um = Conta_Bancaria(id = 1, nome = "Ryan", saldo = 0.73)
+print(conta_um)
