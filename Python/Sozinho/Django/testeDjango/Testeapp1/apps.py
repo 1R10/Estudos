caf0c9e4874a8e89ec220748d4084b075ecfaeb3
1R10 <ryanguilherme2003@gmail.com>
2026-03-13 16:09:51 -0300

    Entendendo a estrutura básica de um projeto Django

---
+++ Python/Sozinho/Django/testeDjango/Testeapp1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Testeapp1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Testeapp1'
---
+++ Python/Sozinho/Django/testeDjango/Testeapp1/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+#Produtos, categorias, users, endereços[...]
+#infos que vao para o database
